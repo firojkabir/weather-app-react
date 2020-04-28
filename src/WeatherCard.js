9fe41072c8bba9497d705aea0c3812aec423e31e
FirozKabir65 <firoj.kabir007@gmail.com>
2020-04-28 16:50:06 +0200

    Created weathercard page and made card html structure

---
+++ src/WeatherCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const WeatherCard = (props) => {
+    return (
+        <div className="card">
+            <h1>Frankfurt</h1>
+            <h3>DE</h3>
+            <img src="" alt="Weather icon"/>
+            <h1>16 °C</h1>
+            <h3>Cloudy</h3>
+        </div>
+    )
+}
+
+export default WeatherCard
